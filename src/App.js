0df404a305c2736c4ff606bae3681d4af758761a
Danny Maier <dannymaier@gmail.com>
2021-02-23 22:14:46 -0800

    i got the app to map across the list of employees and render to the page. success

--- src/App.js
+++ src/App.js
@@ -2,34 +2,36 @@ import React from "react"
 import Header from "./components/Header"
 import Body from "./components/Body"
 import Card from "./components/Card";
+import employeesJSON from "./components/employees.json"
 
-
+const employees = employeesJSON.results
+console.log(employees)
 
 class App extends React.Component{
 
   state={
-    users:[
-
-    ]
-
-
+    employees:employees
   }
 
-render(){
-  return (
-    <div className="container">
-      <Header />
-      <Body/>
-      <Card/>
-
-
-    </div>
-  );
-  }
-
+  render(){
+      return (
+        <div className="container">
+          <Header />
+          <Body/>
+          {this.state.employees.map((employee, index) => {
+            return(
+              <Card
+                key = {employee.login.uuid}
+                id = {employee.login.uuid}
+                {...employee}
+              />
+            )
+          })}
+        </div>
+      );
+    }
 
 }
-
 
 
 export default App;
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,17 +1,34 @@
 import React from 'react'
 import "./style.css";
 
-function Card() {
+function Card(props) {
     return (
         <div>
             <div className="card" style={{width: "18rem"}}>
-                <img className="card-img-top" src="../../../hidden_resources/HeadshotDanMaier.jpg" alt="Card image cap"/>
+                <img className="card-img-top" 
+                    src={props.picture.thumbnail} 
+                    alt={props.name.first}
+                />
                 <div className="card-body">
-                    <h5 className="card-title">Name</h5>
-                    <h6 className="card-subtitle mb-2 text-muted">Title</h6>
-                    <p className="card-text">Status2</p>
-                    <p className="card-text">Status3</p>
-                    <p className="card-text">Status4</p>
+                    <h5 className="card-title">Name: 
+                        {props.name.first} 
+                        {props.name.last}
+                    </h5>
+                    <h6 className="card-subtitle mb-2 text-muted">Branch: 
+                        {props.location.city}
+                    </h6>
+                    <p className="card-text">Email: 
+                        {props.email}
+                    </p>
+                    <p className="card-text">Phone: 
+                        {props.phone}
+                    </p>
+                    <p className="card-text">City: 
+                        {props.location.city}
+                    </p>
+                    <p className="card-text">Country: 
+                        {props.location.country}
+                    </p>
 
                 </div>
             </div>
